bf1986f15299c55139868722e495eede7f259ae3
blockodyssey-uhui <reah@blockodyssey.io>
2022-12-03 18:43:08 +0900

    feat: next-auth 설치

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import NaverProvider from 'next-auth/providers/naver';
+import KakaoProvider from 'next-auth/providers/kakao';
+
+export default NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string
+        }),
+        NaverProvider({
+            clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID as string,
+            clientSecret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET as string
+        }),
+        KakaoProvider({
+            clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID as string,
+            clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET as string
+        })
+    ]
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
-import type { NextPage } from 'next';
+import { getProviders, signIn } from 'next-auth/react';
 
-const Home: NextPage = () => (
-    <div>
-        <button type="button">로그인</button>
-    </div>
-);
+type ProviderNameType = 'google' | 'naver' | 'kakao"';
+type ProviderType = {
+    providers: Record<
+        ProviderNameType,
+        {
+            id: string;
+            name: string;
+            type: string;
+            signinUrl: string;
+            callbackUrl: string;
+        }
+    >;
+};
 
-export default Home;
+export default function SignIn({ providers }: ProviderType) {
+    return (
+        <>
+            {Object.values(providers).map((provider) => (
+                <div key={provider.name}>
+                    <button type="button" onClick={() => signIn(provider.id)}>
+                        Sign in with {provider.name}
+                    </button>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export async function getServerSideProps() {
+    const providers = await getProviders();
+
+    return {
+        props: { providers }
+    };
+}
