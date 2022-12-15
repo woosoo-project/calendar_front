3700f98452013d25be4d1730e7c5034d8434fe09
blockodyssey-uhui <reah@blockodyssey.io>
2022-12-15 20:47:13 +0900

    mod : 네이버 로그인 제거 test

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import NaverProvider from 'next-auth/providers/naver';
+// import NaverProvider from 'next-auth/providers/naver';
 import KakaoProvider from 'next-auth/providers/kakao';
 
 export default NextAuth({
@@ -9,10 +9,10 @@ export default NextAuth({
             clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string
         }),
-        NaverProvider({
-            clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID as string,
-            clientSecret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET as string
-        }),
+        // NaverProvider({
+        //     clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID as string,
+        //     clientSecret: process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET as string
+        // }),
         KakaoProvider({
             clientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID as string,
             clientSecret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET as string
